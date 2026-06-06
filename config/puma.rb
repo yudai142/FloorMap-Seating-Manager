98535d71b25de6f52d6c44b1f551a0347458830c
yudai142 <54573782+yudai142@users.noreply.github.com>
2026-06-06 17:30:25 +0900

    feat(dev): コンテナからアクセスできるよう Rails を 0.0.0.0:3000 で起動する設定を追加

--- config/puma.rb
+++ config/puma.rb
@@ -31,6 +31,9 @@ threads threads_count, threads_count
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 
+# Bind to all interfaces so container port mapping works
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
+
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
